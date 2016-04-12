ea205b992eefbe53c99f1107171f61eee81fb49a
SupingLi <supingli@macbook-pro-2.local>
2016-04-12 17:49:55 +0800

    Initial Commit

---
+++ GUSelection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GUSelection
+//
+//  Created by SupingLi on 4/12/16.
+//  Copyright © 2016 SupingLi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GUSelection/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  GUSelection
+//
+//  Created by SupingLi on 4/12/16.
+//  Copyright © 2016 SupingLi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
